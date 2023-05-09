bcb96243901859ef286e5eb1bd1f78f31189128d
leticialbuk <leticia.albuk@gmail.com>
2023-05-08 23:18:05 -0300

    HERANCA, terceiro pilar de OOP

---
+++ Heranca.cs
@@ -0,0 +1,13 @@
+namespace TestesOOP
+{
+    //Terceiro pilar de OOP
+    public class Pai
+    {
+        //Tudo que estiver dentro da classe Pai irá refletir na classe filho
+    }
+
+    public class Filho : Pai
+    {
+        //Além de ter tudo que a classe Pai tem a classe filho pode ter métodos adicionais 
+    }
+}
