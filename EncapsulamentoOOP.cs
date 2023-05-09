5a34401d1a88d70d00c12c0da13a5fd15b6b4ba1
leticialbuk <leticia.albuk@gmail.com>
2023-05-08 23:04:03 -0300

    ENCAPSULAMENTO, primeiro pilar de OOP

---
+++ EncapsulamentoOOP.cs
@@ -0,0 +1,16 @@
+namespace TestesOOP;
+class Program
+{
+    //Primeiro pilar de OOP
+    // Aqui temos um pilar de encapsulamento, onde agrupamos funções e variaves em um objeto
+    public class Garrafa
+    {   //Propriedades:
+        public int Altura { get; set; }
+        public int Largura { get; set; }
+        public int Capacidade { get; set; }
+
+        //Métodos:
+        public void Abrir() { }
+        public void Fechar() { }
+    }
+}
